09d961bb9a0bbea62e94641390bef10147b4ec03
STS-5 <simon.lombard000@gmail.com>
2019-04-14 16:48:03 +0200

    Fixed logic and styling issues
    Group 3 cards in each row issue fixed
    
    Card body text extending too far fixed

--- Photo_Gallery/index.php
+++ Photo_Gallery/index.php
@@ -34,7 +34,11 @@
   <body>
     <div class="navbar">
       <div class="logoContainer">
+      <div class="logo1"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="mr-2"><path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"></path><circle cx="12" cy="13" r="4"></circle></svg></div>
         <div class="vertical"></div>
+      
+        
+       
         <div class="logo">Photo Gallery</div>
       </div>
     </div>
@@ -42,12 +46,9 @@
     <header></header>
 
       <div class="container">
-
-      
-  
      <?php
        require('includes/connection.php');
-       $z = 0;
+       $z = 1;
 
        //$allowed = array( 'jpeg' , 'png' , 'jpg' );
 
@@ -58,28 +59,83 @@
        $row = mysqli_num_rows($result_questions);
 
        while ($row = mysqli_fetch_array($result_questions)) {
-         
-        if ($z == 3){
-          $z = 0;
+        
+        if ($z == 5){
+          $z = 2;
         }
+
              //for ($z = 0; $z < 3; $z++) {
               // $img = explode('.',$row['Name']);
               // if ($allowed[$z] == $img[1]){
-        if($z == 0){
-
+      if($z == 1){
         echo '
-        
         <div class="card-group">
 
-        <a href="#_" class="lightbox" id="'.$row['Name'].'">
-        <img src="Images/'.$row['Name'].'" />
-      </a>
+          <a href="#_" class="lightbox" id="'.$row['Name'].'">
+            <img src="Images/'.$row['Name'].'" />
+          </a>
 
-        <div class="card">
+          <div class="card">
+            <a href="#'.$row['Name'].'">
+              <img
+                style="object-fit: cover;"
+                class="card-img-top lazy"
+                src="loadirst.jpg"
+                data-src="Images/'.$row['Name'].'"
+                alt="Card image cap"
+              />
+            </a>
+
+            <div class="card-body">
+              <h5 class="card-title">'.$row['Name'].'</h5>
+              <p class="card-text">
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </p> 
+            </div>
+          </div>';
+      }else if($z == 2 || $z == 3){ echo '
+
+          <a href="#_" class="lightbox" id="'.$row['Name'].'">
+            <img src="Images/'.$row['Name'].'" />
+          </a>
+
+          <div class="card">
+            <a href="#'.$row['Name'].'">
+              <img
+              style="object-fit: cover;"
+                class="card-img-top lazy"
+                src="loadirst.jpg"
+                data-src="Images/'.$row['Name'].'"
+                alt="Card image cap"
+              />
+            </a>
+
+            <div class="card-body">
+              <h5 class="card-title">'.$row['Name'].'</h5>
+              <p class="card-text">
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </p>
+                
+            </div>
+          </div>';
+      } else if ($z ==4) {       
+        echo'  
+      </div>
+        <div class="card-group">
+          <a href="#_" class="lightbox" id="'.$row['Name'].'">
+            <img src="Images/'.$row['Name'].'" />
+          </a>
+
+        <div class="card" style="object-fit: cover;">
           <a href="#'.$row['Name'].'">
             <img
-            style="object-fit: cover;"
+            
               class="card-img-top lazy"
+              style="object-fit: cover;"
               src="loadirst.jpg"
               data-src="Images/'.$row['Name'].'"
               alt="Card image cap"
@@ -89,54 +145,11 @@
           <div class="card-body">
             <h5 class="card-title">'.$row['Name'].'</h5>
             <p class="card-text">
-              This is a wider card with supporting text below as a natural
-              lead-in to additional content. This content is a little bit
-              longer.
-            </p>
-            <p class="card-text">
-              <small class="text-muted">Last updated 3 mins ago</small>
-            </p>
-          </div>
-
-          
-      
-          </div>
-          ';
-        $z++;
-        } else {
-                    
-        echo'   
-        
-        <a href="#_" class="lightbox" id="'.$row['Name'].'">
-        <img src="Images/'.$row['Name'].'" />
-      </a>
-                 <div class="card" style="object-fit: cover;">
-                 <a href="#'.$row['Name'].'">
-                   <img
-                   
-                     class="card-img-top lazy"
-                     style="object-fit: cover;"
-                     src="loadirst.jpg"
-                     data-src="Images/'.$row['Name'].'"
-                     alt="Card image cap"
-                   />
-                 </a>
-
-                 <div class="card-body">
-            <h5 class="card-title">'.$row['Name'].'</h5>
-            <p class="card-text">
               Card text can be added here from the back-end
             </p>
-            <p class="card-text">
-              <small class="text-muted">Last updated 3 mins ago</small>
-            </p>
           </div>
-    
-       
-                 
-               </div>';
+        </div>';}
         $z++;
-        }
                       
         } ?>
       </div>
@@ -162,11 +175,12 @@ $(function() {
       document.getElementsByClassName("logo")[0].style.padding = "5px 15px";
       document.getElementsByClassName("logo")[0].style.opacity = "0";
       document.getElementsByClassName("vertical")[0].style.margin =
-        "18px 0px 10px 0px";
+        "13px 0px 13px 0px";
       document.getElementsByClassName("vertical")[0].style.opacity = "0";
-      document.getElementsByClassName("fab")[0].style.margin =
-        "12px 0px 20px 10px";
+      document.getElementsByClassName("logo1")[0].style.padding = "0px 0px 0px 25px";
+
     } else {
+      document.getElementsByClassName("logo1")[0].style.padding = "13px 0px 15px 25px";
       document.getElementsByClassName("navbar")[0].style.maxHeight = "75px";
       document.getElementsByClassName("logo")[0].style.padding = "15px";
       document.getElementsByClassName("logo")[0].style.opacity = "1";
@@ -175,6 +189,7 @@ $(function() {
       document.getElementsByClassName("vertical")[0].style.opacity = "1";
       document.getElementsByClassName("fab")[0].style.margin =
         "22px 0px 20px 10px";
+      
     }
   }
 </script>
